bcedb3f5ee133c68a57861ab34b70af8d4ed0e85
MihaiSavastre <123790347+MihaiSavastre@users.noreply.github.com>
2023-10-13 21:43:42 +0300

    Add files via upload

---
+++ Constants.py
@@ -0,0 +1,14 @@
+import datetime
+
+email_server = "smtp.gmail.com"
+port = 465
+
+sender_email = "#####@###"
+password = "#####"
+
+receiver_email = "####"
+
+
+
+#obsolete
+dates = [datetime.date(2023, 9, 27), datetime.date(2023, 9, 28), datetime.date(2023, 9, 29), datetime.date(2023, 9, 30)]
